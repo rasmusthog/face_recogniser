1965fc5271187200eb9c50244024f826cd280611
Rasmus Vester Thøgersen <rasmusthog@protonmail.com>
2022-04-02 21:11:16 +0200

    Attempted adding profile cascade

--- face_recogniser.py
+++ face_recogniser.py
@@ -5,7 +5,11 @@ import pickle
 
 cap = cv2.VideoCapture(0)
 
+# Load Haar cascades
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+profile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
+
+# Set-up recogniser
 recogniser = cv2.face.LBPHFaceRecognizer_create()
 recogniser.read('models/Rasmus_only.yml')
 
@@ -14,12 +18,16 @@ with open('models/Rasmus_only.pickle', 'rb') as f:
     inv_labels = pickle.load(f)
     labels = {val:key for key, val in inv_labels.items()}
 
+
+# Start video capture
 while True:
     ret, frame = cap.read()
     
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
     faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+    profiles = face_cascade.detectMultiScale(gray, 1.3, 4)
+
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 5)
         roi_gray = gray[y:y+h, x:x+w]
@@ -32,7 +40,9 @@ while True:
             font = cv2.FONT_HERSHEY_SIMPLEX
             cv2.putText(frame, text, (x,y), font, 2, (0,255,0), 5)
 
-            print(conf)
+    
+    for (x, y, w, h) in profiles:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,0), 5)
 
 
 
